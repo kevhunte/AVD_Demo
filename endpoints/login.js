8357eabfa7842d7539547bc1f3873143cc39bcac
kevhunte <kevinmhunte@gmail.com>
2021-03-20 15:13:09 -0400

    Small changes

--- endpoints/login.js
+++ endpoints/login.js
@@ -15,18 +15,18 @@ login.post('/', (req, res) => {
     const username = req.body.username
     const password = req.body.password
 
-    if (typeof password !== 'undefined' && typeof username !== 'undefined') {
+    if (password && username) {
 
         if(username == user.username && password == user.password){
-            res.status(201).send(`Signed in successfully`)
+            res.status(201).json({msg: `Signed in successfully`})
         }
         else {
-            res.status(400).send(`Incorrect username or password`)
+            res.status(400).json({msg: `Incorrect username or password`})
         }
         
     }
     else {
-        res.status(400).send(`Missing username or password`)
+        res.status(400).json({msg: `Missing username or password`})
     }
     
 })
